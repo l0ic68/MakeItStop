1680b5e857b7c671197db2b0e42e301c901cb90d
Loïc Oulerich <maxiloic@gmail.com>
2025-08-24 13:06:18 +0200

    Tag - onCreate

--- app/src/main/java/com/nesta/makeitstop/MainActivity.kt
+++ app/src/main/java/com/nesta/makeitstop/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.nesta.makeitstop
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.nesta.makeitstop.ui.theme.MakeItStopTheme
 
+private const val TAG = "MainActivity"
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
+        Log.d(TAG, "onCreate Called")
+        enableEdgeToEdge()
         setContent {
             MakeItStopTheme {
               /*  Scaffold(
@@ -29,5 +33,34 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart Called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy Called")
     }
 }
