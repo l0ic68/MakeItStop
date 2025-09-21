65b7d46863a8ccf674f538fe65be7aa67e803d2f
Loïc Oulerich <maxiloic@gmail.com>
2025-09-21 18:19:55 +0200

    Create UrgencyPlanScreen.kt

---
+++ app/src/main/java/com/nesta/makeitstop/features/feature_tutorial/ui/UrgencyPlanScreen.kt
@@ -0,0 +1,15 @@
+package com.nesta.makeitstop.features.feature_tutorial.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun UrgencyPlanScreen() {
+
+}
+
+@Preview
+@Composable
+fun preview() {
+    UrgencyPlanScreen()
+}
