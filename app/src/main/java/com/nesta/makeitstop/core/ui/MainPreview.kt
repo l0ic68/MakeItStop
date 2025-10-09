b54411f7f38ec3e8a31c57e56e5f1eedf8f983de
Loïc Oulerich <maxiloic@gmail.com>
2025-10-09 12:44:44 +0200

    change Surface to our mainScaffold

--- app/src/main/java/com/nesta/makeitstop/core/ui/MainPreview.kt
+++ app/src/main/java/com/nesta/makeitstop/core/ui/MainPreview.kt
@@ -2,17 +2,28 @@ package com.nesta.makeitstop.core.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.nesta.makeitstop.navigation.AppNavHost
+import com.nesta.makeitstop.navigation.Routes
+import kotlinx.coroutines.launch
 
 @Composable
 fun PreviewBackground(content: @Composable () -> Unit) {
-    Surface(
+    Scaffold(
+        containerColor = Color.Transparent,
         modifier = Modifier
-            .fillMaxSize()
             .background(
                 Brush.verticalGradient(
                     0f to Color(0xFF0E1B4A),
@@ -20,6 +31,35 @@ fun PreviewBackground(content: @Composable () -> Unit) {
                     1f to Color(0xFF2B2F73)
                 )
             ),
-        color = Color.Transparent
-    ) { content() }
+        topBar = {
+            TopBarNavigation(
+                false,
+                onClick = {
+                }
+            )
+        },
+        bottomBar = {
+
+        }
+    ) { innerPadding ->
+        ModalNavigationDrawer(
+            modifier = Modifier
+                .padding(innerPadding),
+            drawerContent = {
+                ModalDrawerSheet {
+                    Text("Drawer title", modifier = Modifier.padding(16.dp))
+                    HorizontalDivider()
+                    NavigationDrawerItem(
+                        label = { Text(text = "Drawer Item") },
+                        selected = false,
+                        onClick = { /*TODO*/ }
+                    )
+                    // ...other drawer items
+                }
+            },
+            gesturesEnabled = false
+        ) {
+            content()
+        }
+    }
 }
